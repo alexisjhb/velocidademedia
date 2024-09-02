be81548ff345757faa75aa74572a7718e99dc7ef
Alexis Hernández bolivar <169068450+alexisjhb@users.noreply.github.com>
2024-09-02 19:05:14 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa para calcular a velocidade média em km/h");
+ System.out.println();
+ System.out.println("A distancia considerada é:");
+ int distancia = 400;
+ System.out.println(distancia);
+  System.out.println();
+  System.out.println("O tempo aproximado e:");
+ int temp1 = 2;
+   System.out.println(temp1);
+
+    int velocidademedia = distancia/temp;
+
+  System.out.println(velocidademedia)
+
+}
